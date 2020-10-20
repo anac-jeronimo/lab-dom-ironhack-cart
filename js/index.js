eea8b6da1219d18d0f641f3b19c1e7e304bc7db4
Ana Jeronimo <anac_jeronimo@hotmail.com>
2020-10-21 00:17:20 +0100

    done

--- js/index.js
+++ js/index.js
@@ -2,30 +2,44 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  let price = product.querySelector(".price span").innerHTML;
+  let quantity = product.querySelector(".quantity input").value;
+  let subTotalPrice = price * quantity;
+  console.log(subTotalPrice);
+  let subtotalElement = product.querySelector(".subtotal span");
+  subtotalElement.innerHTML = subTotalPrice;
+  return subTotalPrice;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let products = document.getElementsByClassName("product");
+  let totalPrice = 0;
+  for (let i = 0; i < products.length; i++) {
+    totalPrice +=updateSubtotal(products[i]);
+  } console.log(totalPrice);
 
   // ITERATION 3
-  //... your code goes here
+  let totalPriceElement = document.querySelector("#total-value span");
+  totalPriceElement.innerHTML = totalPrice; 
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
+  let target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  const removeButtons = document.getElementsByClassName("btn-remove");
+  for (let i= 0; i < removeButtons.length; i++) {
+    removeButtons[i].addEventListener("click", removeProduct(this));
+    
+  }
 }
 
 // ITERATION 5
@@ -38,5 +52,5 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  
 });
